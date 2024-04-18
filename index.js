2bae4c00436ef08f7bc73e2e8c719c98d2bf907e
Emersson Ramirez <emerssonramirez510@gmail.com>
2024-04-17 20:42:03 -0500

    autenticacion

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const port = 3000
 app.use(express.json())
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
-app.set('view engine','ejs')
-app.set('views','./views')
+app.set('view engine', 'ejs')
+app.set('views', './views')
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
     res.status = 200
-    res.json({'mensaje' : 'Bienvenido a api de juegos'})
+    res.json({ 'mensaje': 'Bienvenido a api de juegos' })
 })
 
 /* app.use(ruta) */
 
 // ruta para acceder a las funciones del UserController
-app.use('/user',rutaUser)
+app.use('/user', rutaUser)
 
 // ruta para acceder a las funciones del PlayController
 app.use('/juegos', playsRoute)
 
-app.use('/documents',(req,res) =>{
+app.use('/documents', (req, res) => {
     res.render('documentacion.ejs')
 })
 
